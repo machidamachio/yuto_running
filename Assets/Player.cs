6b9ca92c41503319100c679a3e5f6cbd509d7bdf
machidamachio <88224480+machidamachio@users.noreply.github.com>
2024-09-01 13:24:29 +0900

    0901

---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKey(KeyCode.LeftArrow))
+       {
+         transform.position += transform.TransformDirection(Vector3.left*3.0f*Time.deltaTime);
+       }
+       
+        if(Input.GetKey(KeyCode.RightArrow))
+       {
+         transform.position += transform.TransformDirection(Vector3.right*3.0f*Time.deltaTime);
+       }
+    }
+
+}
+
